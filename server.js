3047d1f310909da0aa6baa9b0e9a94bbd197a0c1
Kazuhito Hokamura <k.hokamura@gmail.com>
2014-12-21 18:18:58 +0900

    ディレクトリ整理

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 var lastId = 3;
